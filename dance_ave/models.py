66b570d3fa0e9ae923bc4bc965bb87078573304a
Jim Newsome <jim@jimnewsome.net>
2013-04-29 04:55:41 +0000

    song selection by code

--- dance_ave/models.py
+++ dance_ave/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class SongStation(models.Model):
     audio_url = models.URLField()
-    select_code = models.CharField(max_length=10)
-    complete_code = models.CharField(max_length=10)
+    select_code = models.CharField(max_length=10, db_index=True)
+    complete_code = models.CharField(max_length=10, db_index=True)
 
 # Create your models here.
 class Player(models.Model):
--- dance_ave/views.py
+++ dance_ave/views.py
@@ -5,6 +5,8 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.http import HttpResponse
 from tropo import Tropo, Result, Choices
+import dance_ave.models as m
+from django.core.exceptions import ObjectDoesNotExist
 
 class Home(View):
     def get(self, request):
@@ -12,6 +14,26 @@ class Home(View):
 
     def post(self, request):
         t = Tropo()
+        t.ask(choices = Choices(value="[4 DIGITS]"), timeout=60, name="digit", say = "Enter song code")
+        t.on(event = "continue", next ="/django/dance_ave/playcode")
+
 #        t.say("hello django")
-        t.say(['https://s3.amazonaws.com/dance_ave/Ikea.mp3'])
+#        t.say(['https://s3.amazonaws.com/dance_ave/Ikea.mp3'])
+        return HttpResponse(t.RenderJson())
+
+class PlayCode(View):
+    def get(self, request):
+        return HttpResponse("get response")
+
+    def post(self, request):
+        t = Tropo()
+        r = Result(request.body)
+
+        code = r.getValue()
+        try:
+            song = m.SongStation.objects.get(select_code=code)
+            t.say([song.audio_url])
+        except ObjectDoesNotExist:
+            t.say("Sorry, %s is an invalid code!" % code)
+
         return HttpResponse(t.RenderJson())
