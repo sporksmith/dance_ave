1ce682e1662f7d9610caba1aa51b0d72e5cfa076
Jim Newsome <jim@jimnewsome.net>
2013-04-29 03:55:30 +0000

    basic working tropo setup

--- dance_ave/urls.py
+++ dance_ave/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 import dance_ave.views as views
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = patterns('',
-    url(r'^$', views.Home.as_view(), name='home'),
+    url(r'^$', csrf_exempt(views.Home.as_view()), name='home'),
 )
--- dance_ave/views.py
+++ dance_ave/views.py
@@ -4,11 +4,13 @@ from django.views.generic.base import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.http import HttpResponse
+from tropo import Tropo, Result, Choices
 
 class Home(View):
     def get(self, request):
         return HttpResponse("get response")
 
-    @method_decorator( csrf_exempt )
     def post(self, request):
-        return HttpResponse("post response")
+        t = Tropo()
+        t.say("hello django")
+        return HttpResponse(t.RenderJson())
