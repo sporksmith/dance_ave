adf07107a4fa92be819c7ca961201cff2d9fafef
Jim Newsome <jim@jimnewsome.net>
2013-05-07 23:59:00 +0000

    enable basic admin site

---
+++ dance_ave/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from dance_ave.models import Player, SongStation
+
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = (
+            'address',
+#            'completed_stations',
+            )
+
+class SongStationAdmin(admin.ModelAdmin):
+    list_display = (
+            'audio_url',
+            'select_code',
+            )
+
+admin.site.register(Player, PlayerAdmin)
+admin.site.register(SongStation, SongStationAdmin)
